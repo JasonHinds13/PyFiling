dfae3823217ffc07af29cd78ba8951fe6376c313
Brandon Hinds <brandonhinds13@gmail.com>
2016-03-17 10:55:30 -0400

    Updated main.py
    Added folder for programs/executables

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ if not directory.endswith('/'):
     directory += '/'
 
 #General File Type Extentions
+apps      = ['.exe']
 code      = ['.py','.java','.c','.cpp','.rb','.asm','.php','.html',
-             '.css','.js','.lua']
+             '.css','.js','.lua','.jar','.o','.obj']
 
 music     = ['.mp3','.ogg','.wav']
 videos    = ['.mp4','.3gp','.avi']
@@ -23,17 +24,26 @@ documents = ['.docx','.doc','.pdf','.txt','.ppt','.pptx','.ppsx','.pptm',
              '.potx','.potm','.ppam','.ppsm','.sldx','.sldm']
 
 #Hold files of the same type
+app = [] #holds applications
 cod = [] #holds files of type code
 mus = [] #holds files of type music
 vid = [] #holds files of type video
 pic = [] #holds files of type picture
 arc = [] #holds files of type archives
 doc = [] #holds files of type document
+oth = [] #holds files of any other type
 
-#allTypes = [cod, mus, vid, pic, arc, doc]
+#allTypes = [cod, mus, vid, pic, arc, doc, app, oth]
 
 #Check each file in the directory
 for fil in listdir(directory):
+
+    for x in apps:
+        if fil.endswith(x) or fil.endswith(x.upper()):
+            app.append(fil)
+            if not path.exists(directory+'Applications/'):
+                makedirs(directory+'Applications/')
+            rename(directory+fil, directory+'Applications/'+fil)
 
     for x in code:
         if fil.endswith(x) or fil.endswith(x.upper()):
@@ -76,3 +86,14 @@ for fil in listdir(directory):
             if not path.exists(directory+'Documents/'):
                 makedirs(directory+'Documents/')
             rename(directory+fil, directory+'Documents/'+fil)
+
+print ""
+print "How to Find What We Organised:"
+print "Code Folder         - Contains Code, Object Files and jar Files."
+print "Music Folder        - Contains Audio Files."
+print "Videos Folder       - Contains Video Files."
+print "Pictures Folder     - Contains Image Files."
+print "Archives Folder     - Contains rar/zip Files and Other Archives."
+print "Documents Folder    - Contains Powerpoint and Documents from MS Office."
+print "Applications Folder - Contains applications and executable programs."
+print "Other Folder        - Contains files that were not sorted into a folder."
